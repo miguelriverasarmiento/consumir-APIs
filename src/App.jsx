7d7a63481b23934cbf2bb3085f82ca25657b40c3
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-03-02 09:06:48 -0500

    Starting with the course of consuming apis in the best way

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Fetch Pro</h1>
+    </div>
+  )
+}
+
+export default App
