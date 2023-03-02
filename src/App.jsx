bfb45ae9a0b79ff9a7f65c5ece95cbd29536233c
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-03-02 10:44:02 -0500

    Added custom hook useFetch with loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import { useFetch } from './useFetch';
 import './App.css'
 
 function App() {
+  const { data, loading } = useFetch('https://jsonplaceholder.typicode.com/users');
 
   return (
     <div className="App">
       <h1>Fetch Pro</h1>
+      <div className="card">
+        <ul>
+          {loading && <li>Loading...</li>}
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
