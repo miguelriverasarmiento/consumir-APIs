30b3f2a600f65359cc87cdb5c7d130773ea2902d
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-03-02 15:24:32 -0500

    Added error and abortController

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useFetch } from './useFetch';
 import './App.css'
 
 function App() {
-  const { data, loading } = useFetch('https://jsonplaceholder.typicode.com/users');
+  const { data, loading, error, handleCancelRequest } = useFetch('https://jsonplaceholder.typicode.com/users');
 
   return (
     <div className="App">
       <h1>Fetch Pro</h1>
+      <button onClick={handleCancelRequest}>Cancel Request</button>
       <div className="card">
         <ul>
+          {error && <li>Error: {error}</li>}
           {loading && <li>Loading...</li>}
           {data?.map((user) => (
             <li key={user.id}>{user.name}</li>
--- src/useFetch.js
+++ src/useFetch.js
@@ -4,14 +4,35 @@ export function useFetch(url) {
 
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [controller, setController] = useState(null);
 
     useEffect(() => {
+        const abortController = new AbortController();
+        setController(abortController);
         setLoading(true);
-        fetch(url)
+
+        fetch(url, { signal: abortController.signal })
             .then((response) => response.json())
             .then((data) => setData(data))
+            .catch((error) => {
+                if(error.name === "AbortError") {
+                    console.log("Request cancelled")
+                } else {
+                    setError(error)
+                }
+            })
             .finally(() => setLoading(false));
-        }, []);
 
-        return { data, loading };
+        return () => abortController.abort();
+    }, []);
+
+    const handleCancelRequest = () => {
+    if (controller) {
+        controller.abort();
+        setError("Request cancelled");
+        } 
+    };
+
+    return { data, loading, error, handleCancelRequest };
 }
