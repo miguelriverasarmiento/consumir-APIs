ef69479a1861312ab9585cb04145238d772d403b
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-03-03 17:06:33 -0500

    Added with suspense and render unique without useState or useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// Con el custom hook useFetch, error y abortController
+
+/*
 import { useFetch } from './useFetch';
 import './App.css'
 
@@ -19,6 +22,36 @@ function App() {
       </div>
     </div>
   )
+}
+
+export default App*/
+
+/* --------------------------------------- */
+
+// Con suspense
+
+import { Suspense } from 'react';
+import { fetchData } from './fetchData';
+import './App.css'
+
+const apiData = fetchData('https://jsonplaceholder.typicode.com/users')
+
+function App() {
+
+  const data = apiData.read();
+
+  return (
+    <div className="App">
+      <h1>Fetch Pro</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul className="card">
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </Suspense>
+    </div>
+  );
 }
 
 export default App
